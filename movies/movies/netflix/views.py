ecebc5a01f13456f27e9c7ea0f31831fbbf74ad7
omaralkasaby <omaralkasaby899@gmail.com>
2021-03-14 17:14:50 +0200

    second commit

--- movies/movies/netflix/views.py
+++ movies/movies/netflix/views.py
@@ -1,35 +1,50 @@
 from django.shortcuts import render,redirect
 from .forms import movieForm
+from .models import movie 
+from django.contrib.auth.decorators import login_required,permission_required
+
 # Create your views here.
+@login_required
 def index(request):
-    pass
+    movies = movie.object.all()
+
+    return render(request,'netfilx/index.html',{
+        "movies" : movies
+    })
+
+@login_required
+@permission_required('netflix.view_movie')
+def show(request,id):
+    movie = movie.object.get(PK=id)
+    return render(request,'netfilx/show.html',{
+        "movie" : movie
+    })
+@login_required
 def create(request):
     form = movieForm(request.POST or None , request.FILES or None)
     if form.is_valid():
         form.save()
         return redirect('index')
-    return render(request,'netfilx/create.html',{​​​​​
+
+    return render(request,'netfilx/create.html',{
         "form" : form
-    }​​​​​)
+    })
 
-def update(request,id):
-    movie = Movie.objects.get(pk=id)
-    form = MovieForm(request.POST or None ,request.FILES or None,instance=movie)
-    if form.is_vaild():
+
+@login_required
+def update(request):
+    movie = movie.object.get(PK=id)
+    form = movieForm(request.POST or None , request.FILES or None, instance=movie)
+    if form.is_valid():
         form.save()
+        return redirect('index')
 
-        return redirect("index")
-    return render(request,"netflix/create.html",{
-        'form':form
-        "movie":movie
+    return render(request,'netfilx/edit.html',{
+        "form" : form,
+        "movie" : movie
     })
+@login_required
 def delete(request,id):
-    movie=Movies.objects.get(pk=id)
+    movie = movie.object.get(PK=id)
     movie.delete()
-    return redirect("index")
-def show(request,id):
-    movie=Movies.objects.get(pk=id)
-
-    return render(request,"netflix/show.html",{
-        "movies":movie
-    })
+    return redirect('index')
